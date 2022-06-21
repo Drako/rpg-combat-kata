c5ee54b47a8478b42a824bd119fb76a7c4746fb1
Felix Bytow <felix.bytow@naventik.de>
2022-06-21 16:14:56 +0200

    :test_tube: added test for character start values

--- tests/character_tests.cxx
+++ tests/character_tests.cxx
@@ -1,8 +1,17 @@
 #include <gtest/gtest.h>
 
 #include "character.hxx"
+#include "health.hxx"
 
 struct CharacterTests: testing::Test
 {
   rpg::Character character{};
 };
+
+TEST_F(CharacterTests, characterStartsAliveWithFullHealthAtLevel1)
+{
+  int const fullHealth = static_cast<int>(rpg::Health{});
+  EXPECT_EQ(fullHealth, character.health());
+  EXPECT_EQ(1, character.level());
+  EXPECT_TRUE(character.alive());
+}
