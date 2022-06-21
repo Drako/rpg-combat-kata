35ab418e077847d855c80efec8d3f99619a2ef18
Felix Bytow <felix.bytow@naventik.de>
2022-06-21 15:35:14 +0200

    :test_tube: added test for health reduction

--- tests/health_tests.cxx
+++ tests/health_tests.cxx
@@ -4,8 +4,15 @@
 
 static int constexpr FULL_HEALTH = 1000;
 
-TEST(HealthTest, healthStartsAt1000)
+TEST(HealthTests, healthStartsAt1000)
 {
   rpg::Health health;
   EXPECT_EQ(FULL_HEALTH, health);
+}
+
+TEST(HealthTests, healthCanBeReduced)
+{
+  rpg::Health health;
+  health -= 10;
+  EXPECT_EQ(FULL_HEALTH - 10, health);
 }
