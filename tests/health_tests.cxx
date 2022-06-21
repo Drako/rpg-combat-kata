ecf34c674be6c0e2ebbe683ab9e041ba97476847
Felix Bytow <felix.bytow@naventik.de>
2022-06-21 15:42:41 +0200

    :test_tube: added test making sure that dealing negative damage fails

--- tests/health_tests.cxx
+++ tests/health_tests.cxx
@@ -2,6 +2,8 @@
 
 #include "health.hxx"
 
+#include <stdexcept>
+
 static int constexpr FULL_HEALTH = 1000;
 
 struct HealthTests : testing::Test {
@@ -17,4 +19,12 @@ TEST_F(HealthTests, healthCanBeReduced)
 {
   health -= 10;
   EXPECT_EQ(FULL_HEALTH - 10, health);
+}
+
+TEST_F(HealthTests, healthReductionByNegativeValueThrowsException)
+{
+  auto const reduceHealthByNegativeValue = [this]{
+    health -= (-10);
+  };
+  EXPECT_THROW(reduceHealthByNegativeValue(), std::invalid_argument);
 }
