d654c7e65b72f69f7fe56f9756250a9718e99857
Felix Bytow <felix.bytow@naventik.de>
2022-06-21 16:54:11 +0200

    :white_check_mark: characters cannot attack themselves

--- src/combat.cxx
+++ src/combat.cxx
@@ -1,11 +1,13 @@
 #include "combat.hxx"
 #include "character.hxx"
 
+#include <utility>
+
 namespace rpg
 {
 void Combat::attack(const rpg::Character &attacker, rpg::Character &target, int damage)
 {
-  (void)attacker;
-  target.takeDamage(damage);
+  if (std::addressof(target) != std::addressof(attacker))
+    target.takeDamage(damage);
 }
 }
