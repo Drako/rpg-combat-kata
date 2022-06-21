e7ecd2c586e2c5af5b8faa46c2fc5e61b1c0d6e7
Felix Bytow <felix.bytow@naventik.de>
2022-06-21 16:53:14 +0200

    :test_tube: added test making sure characters don't hit themselves

--- tests/combat_tests.cxx
+++ tests/combat_tests.cxx
@@ -10,6 +10,14 @@ TEST(CombatTests, charactersCanAttackEachOther)
   rpg::Character bert{};
   rpg::Combat::attack(ernie, bert, 42);
   rpg::Combat::attack(bert, ernie, 23);
-  EXPECT_EQ(fullHealth-42, bert.health());
-  EXPECT_EQ(fullHealth-23, ernie.health());
+  EXPECT_EQ(fullHealth - 42, bert.health());
+  EXPECT_EQ(fullHealth - 23, ernie.health());
+}
+
+TEST(CombatTests, charactersCannotAttackThemselves)
+{
+  rpg::Character bob{};
+  int const oldHealth = bob.health();
+  rpg::Combat::attack(bob, bob, 10);
+  EXPECT_EQ(oldHealth, bob.health());
 }
