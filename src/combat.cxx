bdbb5e6e741b458d26ecc66a16936fd5a0a12652
Felix Bytow <felix.bytow@naventik.de>
2022-06-21 17:10:50 +0200

    :white_check_mark: strong attackers inflict 50% more damage

--- src/combat.cxx
+++ src/combat.cxx
@@ -3,12 +3,19 @@
 
 #include <utility>
 
+static int constexpr LEVEL_THRESHOLD = 5;
+
 namespace rpg
 {
 void Combat::attack(const rpg::Character &attacker, rpg::Character &target, int const damage)
 {
-  if (std::addressof(target) != std::addressof(attacker))
-    target.takeDamage(damage);
+  if (std::addressof(target) != std::addressof(attacker)) {
+    int const levelDifference = attacker.level() - target.level();
+    if (levelDifference >= LEVEL_THRESHOLD)
+      target.takeDamage(damage + (damage >> 1U));
+    else
+      target.takeDamage(damage);
+  }
 }
 
 void Combat::heal(Character &healer, int const restoration)
