2300387dfa5473b41366802609b9ebe8520d0b01
Felix Bytow <felix.bytow@naventik.de>
2022-06-21 16:57:55 +0200

    :white_check_mark: characters can heal themselves

--- src/combat.cxx
+++ src/combat.cxx
@@ -5,9 +5,14 @@
 
 namespace rpg
 {
-void Combat::attack(const rpg::Character &attacker, rpg::Character &target, int damage)
+void Combat::attack(const rpg::Character &attacker, rpg::Character &target, int const damage)
 {
   if (std::addressof(target) != std::addressof(attacker))
     target.takeDamage(damage);
+}
+
+void Combat::heal(Character &healer, int const restoration)
+{
+  healer.restore(restoration);
 }
 }
--- src/combat.hxx
+++ src/combat.hxx
@@ -7,5 +7,7 @@ class Character;
 struct Combat final
 {
   static void attack(rpg::Character const &attacker, rpg::Character &target, int damage);
+
+  static void heal(rpg::Character & healer, int restoration);
 };
 }
