08009d55dff25bcb2a8ac58224bdb1f10f429e1d
Felix Bytow <felix.bytow@naventik.de>
2022-06-21 16:49:03 +0200

    :test_tube: added test for characters attacking each other

---
+++ tests/combat_tests.cxx
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+#include "character.hxx"
+#include "combat.hxx"
+
+TEST(CombatTests, charactersCanAttackEachOther)
+{
+  int const fullHealth = rpg::Health{};
+  rpg::Character ernie{};
+  rpg::Character bert{};
+  rpg::Combat::attack(ernie, bert, 42);
+  rpg::Combat::attack(bert, ernie, 23);
+  EXPECT_EQ(fullHealth-42, bert.health());
+  EXPECT_EQ(fullHealth-23, ernie.health());
+}
