aefd4308a6448813e69868476d8c981826f79379
Felix Bytow <felix.bytow@naventik.de>
2022-06-21 15:39:18 +0200

    :white_check_mark: implemented health reduction

--- src/health.cxx
+++ src/health.cxx
@@ -1,10 +1,14 @@
 #include "health.hxx"
 
-static int constexpr FULL_HEALTH = 1000;
-
 namespace rpg {
 Health::operator int() const noexcept
 {
-  return FULL_HEALTH;
+  return value;
+}
+
+Health &Health::operator-=(int const damage)
+{
+  value -= damage;
+  return *this;
 }
 }
--- src/health.hxx
+++ src/health.hxx
@@ -4,7 +4,14 @@ namespace rpg
 {
 class Health final
 {
+  static int constexpr FULL_HEALTH = 1000;
+
 public:
+  Health &operator-=(int damage);
+
   operator int() const noexcept;
+
+private:
+  int value{FULL_HEALTH};
 };
 }
