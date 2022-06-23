4bea8fee5a098267f2815c11ffe1e19c698c657e
Felix Bytow <felix.bytow@naventik.de>
2022-06-23 11:23:28 +0200

    :test_tube: added test for damageable things taking damage

--- tests/damagaeble_tests.cxx
+++ tests/damagaeble_tests.cxx
@@ -5,5 +5,10 @@
 
 TEST(DamageableTests, damageableThingsHaveHealth)
 {
-  EXPECT_TRUE((std::is_same<decltype(std::declval<rpg::Damageable&>().health()), rpg::Health>::value));
+  EXPECT_TRUE((std::is_same<decltype(std::declval<rpg::Damageable const&>().health()), rpg::Health>::value));
+}
+
+TEST(DamageableTests, damageableThingsCanTakeDamage)
+{
+  EXPECT_TRUE((std::is_same<decltype(std::declval<rpg::Damageable&>().takeDamage(std::declval<int>())), void>::value));
 }
